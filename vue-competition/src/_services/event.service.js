c2e140954f022e0484453083194f6181f53fa748
Mark the Wang <mark.wang@varlivetech.com>
2020-10-21 11:00:34 +0800

    member manage api docking

--- vue-competition/src/_services/event.service.js
+++ vue-competition/src/_services/event.service.js
@@ -1,5 +1,50 @@
 import axios from 'axios'
 
 export const eventService = { 
-	
+    get_match_teams,
+    change_team_name,
+    create_teams
+}
+
+function get_match_teams(match_id){
+    var param = {
+        "match_id":match_id
+    }
+    return new Promise((resolve, reject) => {
+		axios.post(process.env.VUE_APP_API+'/gaming/v1/event/member_manage/get_match_teams', param).then((resp) => {
+			resolve(resp.data)}).catch((error) => {
+				reject(error)
+			}
+		)
+	})		
+}
+
+function change_team_name(match_id,team_id,name,note){
+    var param = {
+        "match_id":match_id,
+        "team_id":team_id,
+        "name":name,
+        "note":note
+    }
+    return new Promise((resolve, reject) => {
+		axios.post(process.env.VUE_APP_API+'/gaming/v1/event/member_manage/change_team_name', param).then((resp) => {
+			resolve(resp.data)}).catch((error) => {
+				reject(error)
+			}
+		)
+	})		
+}
+
+function create_teams(match_id,input_teams){
+    var param = {
+        "match_id":match_id,
+        "input_teams":input_teams
+    }
+    return new Promise((resolve, reject) => {
+		axios.post(process.env.VUE_APP_API+'/gaming/v1/event/member_manage/create_teams', param).then((resp) => {
+			resolve(resp.data)}).catch((error) => {
+				reject(error)
+			}
+		)
+	})		
 }
--- vue-competition/src/components/page/event/box_taipei_20201025/back/team_manage.vue
+++ vue-competition/src/components/page/event/box_taipei_20201025/back/team_manage.vue
@@ -3,16 +3,17 @@
         <el-card shadow="hover" body-style="padding:10px">
             <div slot="header" class="clearfix">
                 <span style="font-size:16px;">隊伍管理</span>
-                <el-button type="success" size="large" icon="el-icon-plus" class="card-header-r-btn" @click="handleInputTeam"> 新增隊伍</el-button>
+                <el-button v-if="teamData.length == 0" type="success" size="large" icon="el-icon-plus" class="card-header-r-btn" @click="handleInputTeam"> 新增隊伍</el-button>
             </div>
-            <div style="padding:10px;">
+            <div  style="padding:10px;">
                 <el-table 
+                v-if="teamData.length > 0"
                 :data="teamData" 
                 border class="table" 
                 ref="multipleTable">
-                    <el-table-column prop="id" label="團隊ID" align="left" width="100px"></el-table-column>    
-                    <el-table-column prop="name" label="團隊名稱" align="left" width="400px"></el-table-column>  
-                    <el-table-column prop="name" label="備註" align="left" width="auto"></el-table-column>  
+                    <el-table-column prop="id" label="隊伍ID" align="left" width="100px"></el-table-column>    
+                    <el-table-column prop="name" label="隊伍名稱" align="left" width="400px"></el-table-column>  
+                    <el-table-column prop="note" label="備註" align="left" width="auto"></el-table-column>  
                     <el-table-column :label="$t('btn.action')" width="200px" align="center">
                         <template slot-scope="scope">
                             <el-button type="text" icon="el-icon-edit" size="mini"  @click="handleChangeName(scope.$index, scope.row)">隊伍更名</el-button>
@@ -29,6 +30,9 @@
                         <el-form-item label="">
                             <el-input type="textarea" :rows="3" v-model="input_teams"></el-input>
                         </el-form-item>
+                        <el-form-item label="新增隊伍數">
+                            <span>{{team_count}} / 16</span>
+                        </el-form-item>
                     </el-form>
                 </div>
             </div>
@@ -38,7 +42,7 @@
             </span>
         </el-dialog>
 
-        <el-dialog title="隊伍更名" :visible.sync="teamRenameView" width="500px" center>
+        <el-dialog :title=" select_team_id+ ' - 隊伍更名'" :visible.sync="teamRenameView" width="500px" center>
             <div style="margin-top:10px;">
                 <el-form label-position="left" label-width="80px">
                     <el-form-item label="原始隊名">
@@ -61,6 +65,7 @@
 </template>
 <script>
 import Vue from "vue";
+import { eventService } from "@/_services";
 export default {
     name:"DT_setting",
     components: {
@@ -71,20 +76,25 @@ export default {
     },
     data(){
         return {
-            teamData:[
-                {"id":"1","name":"team_1","note":""}
-            ],
+            teamData:[],
             teamRenameView:false,
             teamInputView:false,
             input_teams:"",
 
+            select_team_id:"",
             old_name:"",
             new_name:"",
             team_note:"",
         }
     },
     computed: {
-
+        team_count: function(){
+            if(this.input_teams==""){
+                return 0
+            }else{
+                return this.input_teams.split(";").length
+            }
+        }
     },
     watch: {
         
@@ -103,23 +113,60 @@ export default {
             this.teamInputView=false;
         },
         confirmInputTeam(){
-            this.result_change();
-            this.teamInputView=false;
+            eventService
+            .create_teams(this.match_id,this.input_teams)
+            .then(res=>{
+                if(res.code==1){
+                    this.$message.success("success");
+                    this.getData();
+                    this.result_change();
+                    this.teamInputView=false;
+                }else{
+                    this.$message.warning(res.msg);
+                }
+            });            
         },
 
-        handleChangeName(){
+        handleChangeName(index,row){
+            this.select_team_id=row.id;
+            this.old_name=row.name;
+            this.new_name=row.name;
+            this.team_note=row.note;
             this.teamRenameView=true;
         },
         cancelChangeName(){
             this.teamRenameView=false;
         },
         confirmChangeName(){
-            this.result_change();
-            this.teamRenameView=false;
+            if(this.new_name==""){
+                return this.$message.warning("Team Name cannot empty.");
+            }
+            eventService
+            .change_team_name(this.match_id,this.select_team_id,this.new_name,this.team_note)
+            .then(res=>{
+                if(res.code==1){
+                    this.$message.success("success");
+                    this.getData();
+                    this.result_change();
+                    this.teamRenameView=false;
+                }else{
+                    this.$message.warning(res.msg);
+                }
+            });
+            
         },
 
         getData(){
             console.log(this.match_id);
+            eventService
+            .get_match_teams(this.match_id)
+            .then(res=>{
+                if(res.code==1){
+                    this.teamData = res.data;
+                }else{
+                    this.$message.warning(res.msg);
+                }
+            })
         },
         result_change(){
             this.$emit('change',{"status":true})
